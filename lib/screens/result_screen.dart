abd28039914e683ae3d2a4db5763ae852da35c1e
lyuhyewon <lyuhw11023@gmail.com>
2025-03-07 15:40:38 +0900

    remove: "다시 촬영하기" 버튼 삭제

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:eyeforyou_plus/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
-import 'camera_screen.dart';
 
 class ResultScreen extends StatelessWidget {
   final String imagePath;
@@ -26,18 +25,6 @@ class ResultScreen extends StatelessWidget {
           // 촬영한 이미지 표시
           Expanded(
             child: Image.file(File(imagePath), fit: BoxFit.cover),
-          ),
-          const SizedBox(height: 20),
-
-          // 다시 촬영하기 버튼
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const CameraScreen()),
-              );
-            },
-            child: const Text("다시 촬영하기",),
           ),
           const SizedBox(height: 20),
         ],
