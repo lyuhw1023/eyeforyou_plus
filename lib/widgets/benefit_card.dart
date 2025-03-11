b8d7e971924772ed8265640593a61594b5c58791
lyuhyewon <lyuhw11023@gmail.com>
2025-03-12 00:01:47 +0900

    feat: 혜택 리스트 공통 카드 ui 구현

---
+++ lib/widgets/benefit_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BenefitCard extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const BenefitCard({Key? key, required this.title, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF08436D).withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
