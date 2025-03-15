78c101fc3952570fbeb20627ecc5e32ba14d1fac
lyuhyewon <lyuhw11023@gmail.com>
2025-03-16 00:10:58 +0900

    feat: splash 화면 구현

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:eyeforyou_plus/screens/camera_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3),() {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => const CameraScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/splash.png',
+              width: MediaQuery.of(context).size.width / 3,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
